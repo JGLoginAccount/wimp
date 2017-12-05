5de39454003350c3d062a7f017b7bb1758019050
John Griswold <john.griswold@libertymutual.com>
2017-12-05 15:39:59 -0500

    Initial commit of wimp

---
+++ src/main/java/com/lmig/gfc/wimp/models/Actor.java
@@ -0,0 +1,60 @@
+package com.lmig.gfc.wimp.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Actor {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO) 
+	private int id;
+	@Column(length=75, nullable=false)
+	private String firstName;
+	@Column(length=75, nullable=true)
+	private String lastName;
+	@Column(nullable=true)
+	private Long activeSinceYear;
+	@Column(nullable=true)
+	private Date birthDate;
+	
+	
+	public Actor(String firstName, String lastName, Long activeSinceYear, Date birthDate) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.activeSinceYear = activeSinceYear;
+		this.birthDate = birthDate;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Long getActiveSinceYear() {
+		return activeSinceYear;
+	}
+	public void setActiveSinceYear(Long activeSinceYear) {
+		this.activeSinceYear = activeSinceYear;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+}
